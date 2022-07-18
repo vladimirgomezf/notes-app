f69bda1f24792d4ef78c5355583fe3f42af7e92d
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-07-18 00:33:51 -0400

    change check field name to hecho

--- api/notes_back/models.py
+++ api/notes_back/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Notes(models.Model):
     content = models.CharField('Note Text', max_length=250)
-    check = models.BooleanField('Done', default=False)
+    hecho = models.BooleanField('Done', default=False)
--- api/notes_back/serializers.py
+++ api/notes_back/serializers.py
@@ -5,4 +5,4 @@ from .models import Notes
 class NotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
-        fields = ['content', 'check']
+        fields = ['content', 'hecho']
