013a8ee761e035d90b75fba1bb02936c31e6d6fc
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-07-18 00:14:31 -0400

    Create Notes model

--- api/notes_back/models.py
+++ api/notes_back/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notes(models.Model):
+    content = models.CharField('Note Text', max_length=250)
+    check = models.BooleanField('Done', default=False)
